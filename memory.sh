a2c64c1600dd90f2c5f6c68062d89613f1828720
mohithrr06 <mohithrr@gmail.com>
2023-12-13 20:03:16 +0530

    changed in the script in 2nd line

--- memory.sh
+++ memory.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-data=`df -h . | tail -1 | awk -F " " '{print $5}' | sed 's/%//g'`
+data = `df -h . | tail -1 | awk -F " " '{print $5}' | sed 's/%//g'`
 if [ $data -gt 20 ]
 then
 echo "memory storage crosses 20 ,please take a action"
